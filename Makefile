30e00ea82649b529dc58ecd803ec1811b7462587
Guilherme Wachs <lopespt@users.noreply.github.com>
2014-09-18 14:05:48 -0300

    Database Reader initial commit

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 build:
 	rm -rf build
+	rm -rf release
 	qmake -project -o PhdThesis.pro QT+=core QT+=widgets CONFIG+=c++11
 	cat Makefile_dirs >> PhdThesis.pro
 	qmake -o QMakefile
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,15 @@
 #include <QCoreApplication>
 #include "ComplexNetwork.hpp"
 #include "tests/ComplexNetworkTest.cpp"
+#include "SunDatabaseReader.hpp"
 
 int main(int argc, char **argv){
 
     QCoreApplication a(argc, argv);
-    printf("teste\n");
 
-    initTest( 50000 , 50000000 );
+    SunDatabaseReader r("/Users/wachs/Dropbox/Tese-Guilherme/Implementacoes/Doutorado/SunDatabasePartial");
+    
+    //initTest( 50000 , 50000000 );
 
     return 0;
 }
