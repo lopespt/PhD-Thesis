4d460157700c2971a29b1045c40392c110cfd151
Guilherme Wachs <lopespt@users.noreply.github.com>
2014-10-22 19:24:24 -0200

    installation script

--- installation.sh
+++ installation.sh
@@ -1,3 +1,4 @@
+#Install cmake
 dir=$PWD
 cd /tmp
 wget http://www.cmake.org/files/v3.0/cmake-3.0.2.tar.gz -O cmake.tar.gz
@@ -7,3 +8,14 @@ cd cmake-3.0.2
 make -j5
 sudo make install
 cd $dir
+
+#install vtk v6.1.0
+cd /tmp
+git clone https://github.com/Kitware/VTK.git
+cd VTK
+git checkout tags/v6.1.0
+mkdir vtkBuild
+cd vtkBuild
+cmake ..
+make -j10
+sudo make install
