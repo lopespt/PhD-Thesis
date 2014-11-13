521330490242a5591ed4ee76af8fdc3f7a4b3b7c
Guilherme Wachs <lopespt@users.noreply.github.com>
2014-11-13 12:44:06 -0200

    Random SUN database generator

--- Sources/Experiments/SunRandomFolderGenerator/main.cpp
+++ Sources/Experiments/SunRandomFolderGenerator/main.cpp
@@ -8,15 +8,17 @@ int main(int argc, char*argv[]){
 
     QCommandLineOption iop("input folder", "SUN Database folder","input folder");
     QCommandLineOption oop("output folder", "New random SUN database folder","output folder");
+    QCommandLineOption nop("number", "Number of images in new random SUN database","number");
     QCommandLineParser parser;
     parser.addOption(iop);
     parser.addOption(oop);
+    parser.addOption(nop);
     parser.addHelpOption();
 
     parser.process(app);
 
 
-    SUNRandomFolderGenerator gen(parser.positionalArguments()[0], parser.positionalArguments()[1]);
+    SUNRandomFolderGenerator gen(parser.positionalArguments()[0], parser.positionalArguments()[1], parser.positionalArguments()[2].toInt());
     gen.run();
 
 
