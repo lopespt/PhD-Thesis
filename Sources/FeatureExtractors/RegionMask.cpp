8c2fc7071a302c13dfec1a3b224841f3e52ec26d
Guilherme Wachs <lopespt@users.noreply.github.com>
2015-06-09 00:47:09 -0300

    Added new experiment: OutsideSegmentationEvaluatorTestAddCircle

---
+++ Sources/FeatureExtractors/RegionMask.cpp
@@ -0,0 +1,91 @@
+//
+// Created by Guilherme Wachs on 08/06/15.
+//
+
+#include "RegionMask.h"
+
+RegionMask::RegionMask(const Mat &mask) : Mat(mask) {
+    assert(mask.type() == CV_8UC1);
+    Mask2QPolygon(mask, this->boundary, this->holes);
+}
+
+RegionMask::RegionMask(int height, int width, const QPolygon &boundary, const QList<QPolygon> &holes) : Mat(height,
+                                                                                                            width,
+                                                                                                            CV_8UC1, cvScalarAll(0)),
+                                                                                                        boundary(
+                                                                                                                boundary),
+                                                                                                        holes(holes) {
+    assert(boundary.size() > 0);
+    QPolygon2Mask(*this, boundary, holes);
+}
+
+cv::Mat &RegionMask::QPolygon2Mask(cv::Mat &img, const QPolygon external, const QList<QPolygon> &holes) {
+    vector<vector<cv::Point> > points;
+
+    Utils::QPolygon2CvPointArray(external, points);
+    cv::fillPoly(img, points, cvScalarAll(255), 4);
+
+    for(const QPolygon& hole : holes){
+        Utils::QPolygon2CvPointArray(hole, points);
+        cv::fillPoly(img, points, cvScalarAll(0),4 );
+    }
+    return img;
+}
+
+QPolygon &RegionMask::Mask2QPolygon(const cv::Mat &img, QPolygon &poly, QList<QPolygon> &holes) {
+    poly.clear();
+    vector<vector<cv::Point> > contours;
+    vector<cv::Vec4i> hierarchy;
+    cv::findContours(img.clone(), contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_TC89_KCOS, cv::Point(0, 0));
+
+    //assert(contours.size() > 0);
+
+    if (contours.size() > 0) {
+        for (unsigned int i = 0; i < contours[0].size(); i++) {
+            poly.append(QPoint(contours[0][i].x, contours[0][i].y));
+        }
+
+        int idHole = hierarchy[0][2];
+        for (; idHole != -1; idHole = hierarchy[idHole][0]) {
+            QPolygon pnew;
+            for (unsigned int i = 0; i < contours[idHole].size(); i++) {
+                pnew.append(QPoint(contours[idHole][i].x, contours[idHole][i].y));
+            }
+            holes.append(pnew);
+        }
+    }
+
+    return poly;
+}
+
+const QPolygon &RegionMask::getBoundary() const {
+    return boundary;
+}
+
+const QList<QPolygon> &RegionMask::getHoles() const {
+    return holes;
+}
+
+RegionMask RegionMask::operator+(const RegionMask &other) const{
+    RegionMask n(this->clone());
+    bitwise_or(n, other, n);
+    Mask2QPolygon(n, n.boundary, n.holes);
+    return n;
+}
+
+RegionMask RegionMask::operator-(const RegionMask &other) const{
+    RegionMask n(this->clone());
+    RegionMask temp(other.clone());
+    bitwise_not(temp,temp);
+    bitwise_and(n, temp, n);
+    Mask2QPolygon(n, n.boundary, n.holes);
+    return n;
+}
+
+bool RegionMask::containsPoint(int x, int y) const {
+    return this->at<char>(x,y)>0;
+}
+
+RegionMask::RegionMask(int height, int width) : Mat(height, width, CV_8UC1, cvScalarAll(0)) {
+
+}
---
+++ Sources/FeatureExtractors/RegionMask.h
@@ -0,0 +1,40 @@
+//
+// Created by Guilherme Wachs on 08/06/15.
+//
+
+#ifndef REGIONMASK_H
+#define REGIONMASK_H
+
+#include <opencv/cv.h>
+#include <QPolygon>
+#include <Utilities/Utils.hpp>
+
+using namespace cv;
+
+class RegionMask : public Mat {
+private:
+    QPolygon boundary;
+    QList<QPolygon> holes;
+    static cv::Mat& QPolygon2Mask (cv::Mat& img, const QPolygon external, const QList<QPolygon> & holes);
+    static QPolygon& Mask2QPolygon(const cv::Mat &img, QPolygon& poly,  QList<QPolygon> &holes);
+
+public:
+    RegionMask(){}
+    RegionMask(const Mat&  mask);
+    RegionMask(int height, int width);
+    RegionMask(int height, int width, const QPolygon& boundary, const QList<QPolygon> &holes = QList<QPolygon>());
+
+    const QPolygon &getBoundary() const;
+    const QList<QPolygon> &getHoles() const;
+
+
+    RegionMask operator - (const RegionMask& other) const;
+    RegionMask operator + (const RegionMask& other) const;
+    bool containsPoint(int x, int y) const;
+
+
+
+};
+
+
+#endif //REGIONMASK_H
